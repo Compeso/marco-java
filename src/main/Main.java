1562e292f6e75764f1b319025e65bcec407e236f
lambou <lamine.bourib@googlemail.com>
2022-12-19 10:22:25 +0100

    implement read file

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,23 +11,10 @@ import java.util.List;
 
 public class Main {
     public static void test(String[] args) {
-        System.out.println("Hello Worold!!");
-        String[] filesInDir = new File("./src/main/resources").list();
-        for (String file : filesInDir) {
-            System.out.println("file in Dir: " + file);
-        }
-
         try {
-            List<String> list = readFromInputStreamJava8("./src/main/resources/myfile.txt");
-            
+            List<String> list = readFromFile("./src/main/resources/myfile.txt");
             for(int i=0;i<list.size();i++){
                 String line = list.get(i);
-                System.out.println("line list1 "+i+": "+line);
-            }
-
-            List<String> list2 = readFromFileTryFinally("./src/main/resources/myfile.txt");
-            for(int i=0;i<list2.size();i++){
-                String line = list2.get(i);
                 System.out.println("line list2 "+i+": "+line);
             }
         } catch (IOException e) {
@@ -36,43 +23,24 @@ public class Main {
 
     }
 
-    public static List<String> readFromInputStreamJava8(String path)
-            throws IOException {
-        File file = new File(path);
-        InputStream inputStream = new FileInputStream(file);
-        List<String> list = new ArrayList<>();
-        // try with resource => schließt die BufferReader automatisch nach dem Try block
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                list.add(line);
-            }
-        } // hier ist der unsichtbare Code. Hier wird der Buffered Reader geschlossen
-        return list;
-    }
 
-    public static List<String> readFromFileTryFinally(String path)
+    public static List<String> readFromFile(String path)
             throws IOException {
         // StringBuilder ist sowas wie eine Liste. Hier kannst du alle Strings anhängen.
         BufferedReader br = null;
-        List<String> list = new ArrayList<>();
+        //List<String> list = ?
         // try finally => finally ist wie ein Auffangnetz, der immer ausgefuert wird
         try {
-            File file = new File(path);
-            InputStream inputStream = new FileInputStream(file);
-            br = new BufferedReader(new InputStreamReader(inputStream));
-            String line;
-            while ((line = br.readLine()) != null) {
-                list.add(line);
-            }
+            //File Objekt und InputStream Anlegen und BufferedReader erstellen
+            
+            //mit BufferedReader Zeile für Zeile lesen per while loop
+            
         } finally {
             // wird immer ausgeführt, bei Fehler im Try-Block oder wenn der Try-Block
             // verlassen wird
-            if (br != null) {
-                br.close();
-            }
+            //bufferedReader wieder schließen per Close-Methode
         }
-        return list;
+        return null;
     }
 
 }
