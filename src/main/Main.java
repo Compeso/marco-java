d764341e55f8ee732c4a46e1b78ab7a5c1933529
lambou <lamine.bourib@googlemail.com>
2022-12-23 10:06:35 +0100

    write file

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,26 +11,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void test(String[] args) {
-        System.out.println("Hello Worold!!");
-        String[] filesInDir = new File("./src/main/resources").list();
+    public static final String NEW_LINE="\n";
+    public static final String OPTION_EXIT="EXIT!!";
+    public static final String ENTER_LINE_TEXT="Please enter new line and finish with enter. For Exit enter \""+OPTION_EXIT+"\":";
+    public static final String ENTER_FILE_TEXT="Please enter the file name and finish with enter:";
+    public static final String RESOURCE_PATH="./src/main/resources/";
+
+    
+
+    public static void main(String[] args) {
+        String[] filesInDir = new File(RESOURCE_PATH).list();
         for (String file : filesInDir) {
             System.out.println("file in Dir: " + file);
         }
 
         try {
-            List<String> list = readFromInputStreamJava8("./src/main/resources/myfile.txt");
+            List<String> list = readFromInputStreamJava8(RESOURCE_PATH+"myfile.txt");
             
             for(int i=0;i<list.size();i++){
                 String line = list.get(i);
                 System.out.println("line list1 "+i+": "+line);
             }
 
-            List<String> list2 = readFromFileTryFinally("./src/main/resources/myfile.txt");
+            List<String> list2 = readFromFileTryFinally(RESOURCE_PATH+"myfile.txt");
             for(int i=0;i<list2.size();i++){
                 String line = list2.get(i);
                 System.out.println("line list2 "+i+": "+line);
             }
+
+
+            String option = "";
+            String filenameToWrite = RESOURCE_PATH + readInput(ENTER_FILE_TEXT);
+            while(!option.equals(OPTION_EXIT))
+            {
+                option = readInput(ENTER_LINE_TEXT);
+                if(option!=null && option.length() > 0&& !option.equals(OPTION_EXIT)){
+                    writeFile(filenameToWrite,option);
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -73,6 +93,57 @@ public class Main {
             }
         }
         return list;
+    }
+
+    public static void writeFile(String path,String text){
+         // Content to be assigned to a file
+        // Custom input just for illustration purposes
+ 
+        // Try block to check if exception occurs
+        try {
+ 
+            // Create a FileWriter object
+            // to write in the file
+            FileWriter fWriter = new FileWriter(
+                path,true);
+                
+ 
+            // Writing into file
+            // Note: The content taken above inside the
+            // string
+            fWriter.write(text+NEW_LINE);
+ 
+            // Printing the contents of a file
+            System.out.println(text);
+ 
+            // Closing the file writing connection
+            fWriter.close();
+ 
+            
+        }
+ 
+        // Catch block to handle if exception occurs
+        catch (IOException e) {
+ 
+            // Print the exception
+            e.printStackTrace();
+        }
+    
+    }
+
+    public static String readInput(String inputText) throws IOException{
+        
+        System.out.println(inputText);
+        // Enter data using BufferReader
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+ 
+        // Reading data using readLine
+        String name = reader.readLine();
+ 
+        // Printing the read line
+        System.out.println("Entered text:"+name);
+        return name;
     }
 
 }
